7fcfdd137969e0bd85b97a81f25ddabc258d9cef
Evey <evey@evey.cc>
2026-07-17 00:00:40 +0200

    games: responsive UIs at any viewport (canvas DPR/resize, fluid HUD, both orientations) + meta hardening

--- browser-games/service-worker.js
+++ browser-games/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'games-v3';
+const CACHE_NAME = 'games-v4';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
